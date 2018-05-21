d6d901651cdd1b42ce947cd908509c362a4fb2e7
Andrew Mamroth <andrew_mamroth@berkeley.edu>
2018-05-21 19:57:02 -0400

    Create docker_install.sh

---
+++ Week_3/docker_install.sh
@@ -0,0 +1,17 @@
+apt-get update
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+    
+# add the docker repo    
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+ 
+# install it
+apt-get update
+apt-get install docker-ce
