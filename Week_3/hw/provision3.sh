ec3219c1fc84696396f297131e222ae4d8c8f6be
Andrew Mamroth <andrew_mamroth@berkeley.edu>
2018-05-22 17:09:32 -0400

    Update provision3.sh

--- Week_3/hw/provision3.sh
+++ Week_3/hw/provision3.sh
@@ -1,4 +1,4 @@
 ssh-keygen -f ~/.ssh/id_rsa_3 -b 2048 -t rsa -C 'made for HW3'
 slcli sshkey add -f ~/.ssh/id_rsa_3.pub --note 'added during HW 3' homework_3
 
-slcli vs create -d dal09 --os UBUNTU_LATEST_64 --cpu 2 --memory 2048 --hostname mosquito --domain someplace.net --key homework_3
+slcli vs create -d hou02 --os UBUNTU_LATEST_64 --cpu 2 --memory 2048 --hostname mosquito --domain someplace.net --key homework_3
