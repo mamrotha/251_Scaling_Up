b023b99300dfddffcc11a23289939ea9aa1fffd2
Andrew Mamroth <andrew_mamroth@berkeley.edu>
2018-05-22 09:56:48 -0400

    Update saltmaster_setup.sh

--- Week_2/saltmaster_setup.sh
+++ Week_2/saltmaster_setup.sh
@@ -7,8 +7,8 @@ pip2 install softlayer
 
 cat <<EOT >> ~/.softlayer
 [softlayer]
-username = YOUR_SL_API_ID
-api_key = YOUR_SL_API_KEY
+username = SL1655535
+api_key = 96a97f03850db360f57e21ed7b05dfd3080050cf7f9c8d832627b90e66a3a346
 endpoint_url = https://api.softlayer.com/xmlrpc/v3.1/
 timeout = 0
 EOT
