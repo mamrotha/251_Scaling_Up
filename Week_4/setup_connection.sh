618942cf2d46029f5a26184a99b1b3d23b949e46
Andrew Mamroth <andrew_mamroth@berkeley.edu>
2018-05-25 11:04:54 -0400

    Update setup_connection.sh

--- Week_4/setup_connection.sh
+++ Week_4/setup_connection.sh
@@ -1,8 +1,11 @@
 #gpfs1
 cat .ssh/id_rsa_3.pub | ssh root@host 'cat >> .ssh/authorized_keys'
+ssh root@host "chmod 700 .ssh; chmod 640 .ssh/authorized_keys"
 
 #gpfs2
 cat .ssh/id_rsa_3.pub | ssh root@host 'cat >> .ssh/authorized_keys'
+ssh root@host "chmod 700 .ssh; chmod 640 .ssh/authorized_keys"
 
 #gpfs3
 cat .ssh/id_rsa_3.pub | ssh root@host 'cat >> .ssh/authorized_keys'
+ssh root@host "chmod 700 .ssh; chmod 640 .ssh/authorized_keys"
