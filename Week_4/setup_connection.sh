33e7bf006249594c7422fa939a70d0fdd2ab3714
Andrew Mamroth <andrew_mamroth@berkeley.edu>
2018-05-25 14:50:07 -0400

    Update setup_connection.sh

--- Week_4/setup_connection.sh
+++ Week_4/setup_connection.sh
@@ -3,6 +3,8 @@ cat /root/.ssh/id_rsa.pub | ssh root@host 'cat >> /root/.ssh/authorized_keys'
 ssh root@host "chmod 700 .ssh; chmod 640 .ssh/authorized_keys"
 scp setup_node.sh root@host:setup_node.sh
 
+wget http://1d7c9.http.dal05.cdn.softlayer.net/icp-artifacts/GPFS_4.1_STD_LSX_QSG.tar
+
 #gpfs2
 cat /root/.ssh/id_rsa.pub | ssh root@host 'cat >> /root/.ssh/authorized_keys'
 ssh root@host "chmod 700 .ssh; chmod 640 .ssh/authorized_keys"
