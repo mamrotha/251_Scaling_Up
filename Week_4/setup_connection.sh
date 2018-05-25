e4c0e0c9f4c21c439f4c6825d2131dfd3be57014
Andrew Mamroth <andrew_mamroth@berkeley.edu>
2018-05-25 14:42:33 -0400

    Update setup_connection.sh

--- Week_4/setup_connection.sh
+++ Week_4/setup_connection.sh
@@ -1,14 +1,14 @@
 #gpfs1
-cat .ssh/id_rsa_3.pub | ssh root@host 'cat >> .ssh/authorized_keys'
+cat /root/.ssh/id_rsa.pub | ssh root@host 'cat >> /root/.ssh/authorized_keys'
 ssh root@host "chmod 700 .ssh; chmod 640 .ssh/authorized_keys"
 scp setup_node.sh root@host:setup_node.sh
 
 #gpfs2
-cat .ssh/id_rsa_3.pub | ssh root@host 'cat >> .ssh/authorized_keys'
+cat /root/.ssh/id_rsa.pub | ssh root@host 'cat >> /root/.ssh/authorized_keys'
 ssh root@host "chmod 700 .ssh; chmod 640 .ssh/authorized_keys"
 scp setup_node.sh root@host:setup_node.sh
 
 #gpfs3
-cat .ssh/id_rsa_3.pub | ssh root@host 'cat >> .ssh/authorized_keys'
+cat /root/.ssh/id_rsa.pub | ssh root@host 'cat >> /root/.ssh/authorized_keys'
 ssh root@host "chmod 700 .ssh; chmod 640 .ssh/authorized_keys"
 scp setup_node.sh root@host:setup_node.sh
