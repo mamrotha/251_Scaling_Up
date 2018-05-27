f6bbba229d8475d83ee7dfa04c85a7ef23038f86
Andrew Mamroth <andrew_mamroth@berkeley.edu>
2018-05-27 09:48:18 -0400

    Update and rename data_pull_process.sh to data_pull_process1.sh

--- Week_4/data_pull_process1.sh
+++ Week_4/data_pull_process1.sh
@@ -1,10 +1,10 @@
 for i in {0..99}
 do
-  echo "starting $i"
   wget http://storage.googleapis.com/books/ngrams/books/googlebooks-eng-all-2gram-20090715-$i.csv.zip
   unzip googlebooks-eng-all-2gram-20090715-$i.csv.zip
   rm -f googlebooks-eng-all-2gram-20090715-$i.csv.zip
-  awk -f cleanup.awk googlebooks-eng-all-2gram-20090715-$i.csv >> two_gram_$i.csv
+  echo "---------------------cleaning--------------------"
+  awk -f cleanup_raw.awk googlebooks-eng-all-2gram-20090715-$i.csv >> two_gram_$i.csv
   rm -f googlebooks-eng-all-2gram-20090715-$i.csv
 done
   
