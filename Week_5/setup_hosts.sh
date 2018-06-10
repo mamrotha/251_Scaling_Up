4f9ccee92f92f5f1f552a6f8c0149e8a48684d96
Andrew Mamroth <andrew_mamroth@berkeley.edu>
2018-06-10 12:21:21 -0400

    Update setup_hosts.sh

--- Week_5/setup_hosts.sh
+++ Week_5/setup_hosts.sh
@@ -1,4 +1,8 @@
+rm -f /etc/hosts
+
+cat <<EOT >> /etc/hosts
 127.0.0.1 localhost.localdomain localhost
-50.22.13.216 master.hadoop.mids.lulz.bz master
-50.22.13.194 slave1.hadoop.mids.lulz.bz slave1
-50.22.13.217 slave2.hadoop.mids.lulz.bz slave2
+50.97.248.172 master.hadoop.mids.lulz.bz master
+50.97.248.170 slave1.hadoop.mids.lulz.bz slave1
+169.53.129.180 slave2.hadoop.mids.lulz.bz slave2
+EOT
