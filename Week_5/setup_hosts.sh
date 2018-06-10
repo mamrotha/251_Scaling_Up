0d5aaec4ac57573213aceec581b10d6cc51208bf
Andrew Mamroth <andrew_mamroth@berkeley.edu>
2018-06-10 12:16:55 -0400

    Create setup_hosts.sh

---
+++ Week_5/setup_hosts.sh
@@ -0,0 +1,4 @@
+127.0.0.1 localhost.localdomain localhost
+50.22.13.216 master.hadoop.mids.lulz.bz master
+50.22.13.194 slave1.hadoop.mids.lulz.bz slave1
+50.22.13.217 slave2.hadoop.mids.lulz.bz slave2
