74a63cf0fca7dff24edbb8e404e968a885c2fc4b
Andrew Mamroth <andrew_mamroth@berkeley.edu>
2018-06-14 17:59:36 -0400

    Update setup_hosts.sh

--- Week_5/setup_hosts.sh
+++ Week_5/setup_hosts.sh
@@ -1,12 +1,21 @@
-scp setup_hosts.sh root@50.97.248.172:/root/setup_hosts.sh
-scp setup_hosts.sh root@50.97.248.170:/root/setup_hosts.sh
-scp setup_hosts.sh root@169.53.129.180:/root/setup_hosts.sh
+scp setup_hosts.sh root@198.11.209.166:/root/setup_hosts.sh
+scp setup_hosts.sh root@50.97.255.58:/root/setup_hosts.sh
+scp setup_hosts.sh root@198.11.209.165:/root/setup_hosts.sh
+
+ssh root@198.11.209.166 'chmod +x /root/setup_hosts.sh'
+ssh root@50.97.255.58 'chmod +x /root/setup_hosts.sh'
+ssh root@198.11.209.165 'chmod +x /root/setup_hosts.sh'
+
+ssh root@198.11.209.166 'bash -s' < /root/setup_hosts.sh
+ssh root@50.97.255.58 './root/setup_hosts.sh'
+ssh root@198.11.209.165 './root/setup_hosts.sh'
 
 rm -f /etc/hosts
 
 cat <<EOT >> /etc/hosts
 127.0.0.1 localhost.localdomain localhost
-50.97.248.172 master.hadoop.mids.lulz.bz master
-50.97.248.170 slave1.hadoop.mids.lulz.bz slave1
-169.53.129.180 slave2.hadoop.mids.lulz.bz slave2
+198.11.209.166 master.hadoop.mids.lulz.bz master
+50.97.255.58 slave1.hadoop.mids.lulz.bz slave1
+198.11.209.165 slave2.hadoop.mids.lulz.bz slave2
 EOT
+
