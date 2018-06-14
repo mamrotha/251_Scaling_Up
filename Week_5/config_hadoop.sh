723c821d91017737c86cda2cd7d4d7d1ab5f3eaf
Andrew Mamroth <andrew_mamroth@berkeley.edu>
2018-06-14 18:08:13 -0400

    Create config_hadoop.sh

---
+++ Week_5/config_hadoop.sh
@@ -0,0 +1,48 @@
+echo "export JAVA_HOME=\"$(readlink -f $(which javac) | grep -oP '.*(?=/bin)')\"" >> ~/.bash_profile
+
+cat <<\EOF >> ~/.bash_profile
+export HADOOP_HOME=/usr/local/hadoop
+export HADOOP_MAPRED_HOME=$HADOOP_HOME
+export HADOOP_HDFS_HOME=$HADOOP_HOME
+export YARN_HOME=$HADOOP_HOME
+export PATH=$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin
+EOF
+
+source ~/.bash_profile
+$JAVA_HOME/bin/java -version
+
+cd $HADOOP_HOME/etc/hadoop
+
+echo "export JAVA_HOME=\"$JAVA_HOME\"" > ./hadoop-env.sh
+
+#Adjust core-site.xml file
+rm -f core-site.xml
+
+cat <<\EOF >> core-site.xml
+<?xml version="1.0"?>
+<configuration>
+  <property>
+    <name>fs.defaultFS</name>
+    <value>hdfs://master/</value>
+  </property>
+</configuration>
+EOT
+
+
+rm -f yarn-site.xml
+
+cat <<\EOF >> yarn-site.xml
+<?xml version="1.0"?>
+<configuration>
+  <property>
+    <name>yarn.resourcemanager.hostname</name>
+    <value>master</value>
+  </property>
+  <property>
+    <name>yarn.nodemanager.aux-services</name>
+    <value>mapreduce_shuffle</value>
+  </property>
+</configuration>
+EOT
+
+
