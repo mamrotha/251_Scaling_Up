e4c93134455573c462277a7931bdb974ba93324a
Andrew Mamroth <andrew_mamroth@berkeley.edu>
2018-06-14 18:11:46 -0400

    Update config_hadoop.sh

--- Week_5/config_hadoop.sh
+++ Week_5/config_hadoop.sh
@@ -17,7 +17,6 @@ echo "export JAVA_HOME=\"$JAVA_HOME\"" > ./hadoop-env.sh
 
 #Adjust core-site.xml file
 rm -f core-site.xml
-
 cat <<\EOF >> core-site.xml
 <?xml version="1.0"?>
 <configuration>
@@ -28,9 +27,8 @@ cat <<\EOF >> core-site.xml
 </configuration>
 EOF
 
-
+#Adjust yarn-site.xml
 rm -f yarn-site.xml
-
 cat <<\EOF >> yarn-site.xml
 <?xml version="1.0"?>
 <configuration>
@@ -42,7 +40,43 @@ cat <<\EOF >> yarn-site.xml
     <name>yarn.nodemanager.aux-services</name>
     <value>mapreduce_shuffle</value>
   </property>
+  <property>
+    <name>yarn.nodemanager.resource.cpu-vcores</name>
+    <value>2</value>
+  </property>
+  <property>
+    <name>yarn.nodemanager.resource.memory-mb</name>
+    <value>4096</value>
+  </property>
 </configuration>
 EOF
+
+cat <<\EOF >> mapred-site.xml
+<?xml version="1.0"?>
+<configuration>
+  <property>
+    <name>mapreduce.framework.name</name>
+    <value>yarn</value>
+  </property>
+</configuration>
+EOF
+
+cat <<\EOF >> hdfs-site.xml
+<?xml version="1.0"?>
+<configuration>
+  <property>
+    <name>dfs.datanode.data.dir</name>
+    <value>file:///data/datanode</value>
+  </property>
+  <property>
+    <name>dfs.namenode.name.dir</name>
+    <value>file:///data/namenode</value>
+  </property>
+  <property>
+    <name>dfs.namenode.checkpoint.dir</name>
+    <value>file:///data/namesecondary</value>
+  </property>
+  </configuration>
+  EOF
 
 
